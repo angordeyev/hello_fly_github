ccd4feacfdde6dd87c53a93a619654ac29d8be05
angordeyev <angordeyev@gmail.com>
2022-08-17 14:17:15 +0300

    GitHub deploy

---
+++ test/support/fixtures/users_fixtures.ex
@@ -0,0 +1,21 @@
+defmodule HelloFlyGithub.UsersFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `HelloFlyGithub.Users` context.
+  """
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        email: "some email",
+        name: "some name"
+      })
+      |> HelloFlyGithub.Users.create_user()
+
+    user
+  end
+end
